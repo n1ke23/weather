acdb10ab6629f8eed7b960878b49a66dcbc867a6
Aleksandr Jashin <67903349+n1ke23@users.noreply.github.com>
2020-10-14 14:00:47 +0300

    добавил дату и время

--- src/js/renderApi.js
+++ src/js/renderApi.js
@@ -12,25 +12,30 @@ refs.form.addEventListener('submit', (e) => {
     fetch.city = e.currentTarget.city.value;
     fetch.fetchWeather().then((data) => {
         // console.log(data);
-        //  console.log(new Date((data.sys.sunset * 1000) - (data.sys.sunrise * 1000)).getHours());
         // refs.item.innerHTML += `<p>Дата: ${currentTime}</p><p>Восход солнца: ${sunriseTime}</p><p>Закат солнца: ${sunsetTime}</p>`
         unicRender(data)
         
     });
 })
-        function unicRender(data) {
-            data.dt = new Date(data.dt * 1000);
-            data.time = data.dt.getFullYear()
-            // console.log(currentTime);
-            data.sys.sunrise = new Date(data.sys.sunrise * 1000);
-            data.sys.sunset = new Date(data.sys.sunset * 1000);
-            render({ data })
-        
-            // document.querySelector('.dtJs').textContent = currentTime
-            // console.log(currentTime);
-            // console.log(data.dt);
-        
-        }
+function unicRender(data) {
+    // data.dtNow = new Date(data.dt * 1000);
+    // data.nowDay = data.dtNow.getDay();
+    // data.nowMonth = data.dtNow.getMonth() + 1;
+    // data.nowYear = data.dtNow.getFullYear();
+    data.dtNow = `${new Date(data.dt * 1000).getDate()}.${new Date(data.dt * 1000).getMonth() + 1}.${new Date(data.dt * 1000).getFullYear()}`;
+    // data.sys.sunriseNew = new Date(data.sys.sunrise * 1000);
+    // data.sys.sunriseHours = data.sys.sunriseNew.getHours();
+    // data.sys.sunriseMinutes = data.sys.sunriseNew.getMinutes()
+    data.sys.sunriseNew = `${new Date(data.sys.sunrise * 1000).getHours()}:${new Date(data.sys.sunrise * 1000).getMinutes()}`
+    // data.sys.sunsetNew = new Date(data.sys.sunset * 1000);
+    // data.sys.sunsetHours = data.sys.sunsetNew.getHours()
+    // data.sys.sunsetMinutes =data.sys.sunsetNew.getMinutes()
+    data.sys.sunsetNew = `${new Date(data.sys.sunset * 1000).getHours()}:${new Date(data.sys.sunset * 1000).getMinutes()}`
+    //  console.log(new Date((data.sys.sunset * 1000) - (data.sys.sunrise * 1000)).getHours());
+    data.sys.longDay = `${new Date((data.sys.sunset * 1000) - (data.sys.sunrise * 1000)).getHours()}`
+    
+    render({ data })
+}
 function render(data) {
     refs.item.innerHTML = ""
     refs.input.value = ''
