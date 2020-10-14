9ffd2a5ec222dafdf84d403c0f5d22ab0a37dcd3
Aleksandr Jashin <67903349+n1ke23@users.noreply.github.com>
2020-10-14 11:25:25 +0300

    add my cod

--- src/js/renderApi.js
+++ src/js/renderApi.js
@@ -12,13 +12,25 @@ refs.form.addEventListener('submit', (e) => {
     fetch.city = e.currentTarget.city.value;
     fetch.fetchWeather().then((data) => {
         // console.log(data);
-        // console.log(new Date((data.sys.sunset * 1000) - (data.sys.sunrise * 1000)).getHours());
+        //  console.log(new Date((data.sys.sunset * 1000) - (data.sys.sunrise * 1000)).getHours());
         // refs.item.innerHTML += `<p>Дата: ${currentTime}</p><p>Восход солнца: ${sunriseTime}</p><p>Закат солнца: ${sunsetTime}</p>`
-        render({ data })
         unicRender(data)
-
+        
     });
 })
+        function unicRender(data) {
+            data.dt = new Date(data.dt * 1000);
+            data.time = data.dt.getFullYear()
+            // console.log(currentTime);
+            data.sys.sunrise = new Date(data.sys.sunrise * 1000);
+            data.sys.sunset = new Date(data.sys.sunset * 1000);
+            render({ data })
+        
+            // document.querySelector('.dtJs').textContent = currentTime
+            // console.log(currentTime);
+            // console.log(data.dt);
+        
+        }
 function render(data) {
     refs.item.innerHTML = ""
     refs.input.value = ''
@@ -26,14 +38,4 @@ function render(data) {
     refs.item.insertAdjacentHTML('afterbegin', items)
     // sunriseJs.insertAdjacentHTML('beforeend', sunriseTime)
     // sunsetJs.insertAdjacentHTML('beforeend', sunsetTime)
-}
-function unicRender(data) {
-    currentTime = new Date(data.dt * 1000);
-    sunriseTime = new Date(items.sys.sunrise * 1000);
-    sunsetTime = new Date(items.sys.sunset * 1000);
-
-    document.querySelector('.dtJs').textContent = currentTime
-    console.log(currentTime);
-    // console.log(data.dt);
-
 }
